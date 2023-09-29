2d7590332ad4dc668960b9e14183e3165a699e89
karthikvijayjirlimath <44266740+karthikvijayjirlimath@users.noreply.github.com>
2023-09-29 21:50:09 +0530

    AWS launch template

--- main.tf
+++ main.tf
@@ -2,35 +2,12 @@ provider "aws" {
   region = "us-west-1"
 }
 
-data "aws_ami" "Amazon_Linux" {
-  most_recent = true
-  owners = ["amazon"]
+resource "aws_launch_template" "casper_launch_template" {
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
-resource "aws_launch_configuration" "capser_launch_configuration" {
-  name = "ASG002_launch_config"
-  image_id = data.aws_ami.Amazon_Linux.image_id
-  instance_type = "t2.nano"
-
+  instance_type = "t2.micro"
   metadata_options {
-    http_endpoint = "enabled"
-    http_tokens = "required"
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
   }
-  
-}
-
-output "Launch_configuration_name" {
-  description = "Amazon Linux imageID"
-  value = aws_launch_configuration.capser_launch_configuration.name
 }
 
