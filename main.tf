f9de0d9cdb0c63ae9222045292161df38dbfe63b
karthikvijayjirlimath <44266740+karthikvijayjirlimath@users.noreply.github.com>
2023-10-11 14:47:32 +0530

    Update aws_iam_006_user.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,44 @@
-provider "aws" {
-  region = "us-west-1"
-}
-
-resource "aws_launch_template" "casper_launch_template" {
-
-  instance_type = "t2.micro"
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.20"
+    }
   }
 }
 
+provider "aws" {
+    region = "us-west-1"
+}
+
+resource "aws_iam_user" "casper_iam" {
+   name = "casper_iam"
+   path = "/system/"
+
+   tags = {
+    environment = "test"
+   }
+}
+
+data "aws_iam_policy_document" "casper_policy" {
+  version = "2012-10-17"
+  
+  statement {
+    sid = "AWS IAM overprivileged access"
+    principals {
+      type = "AWS"
+      identifiers = ["${aws_iam_user.casper_iam.arn}"]
+    }
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+    
+  }
+}
+
+resource "aws_iam_user_policy" "casper_testing_policy" {
+  name = "casper_check_policy"
+  user = aws_iam_user.casper_iam.name
+
+  policy = data.aws_iam_policy_document.casper_policy.json
+}
